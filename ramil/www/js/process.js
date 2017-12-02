447071199a4ff1b5d70d435a890e1546ed3143bb
ramildelacruz <ramildelacruz641@gmail.com>
2017-12-02 14:05:34 +0800

    ramil

---
+++ ramil/www/js/process.js
@@ -0,0 +1,302 @@
+
+
+var a1 = new Audio();
+a1.src = "audio/bats.mp3";
+function P1() {
+    a1.play();
+}
+
+
+var polarbear = new Audio();
+polarbear.src ="audio/polarbear.mp3";
+function P2(){
+	polarbear.play();
+}
+
+var camel = new Audio();
+camel.src ="audio/camel.mp3";
+function P3(){
+	camel.play();
+}
+
+var carabao = new Audio();
+carabao.src ="audio/carabao.mp3";
+function P4(){
+	carabao.play();
+}
+
+var cat = new Audio();
+cat.src ="audio/cat.mp3";
+function P5(){
+	cat.play();
+}
+
+var chick = new Audio();
+chick.src ="audio/chick.mp3";
+function P6(){
+	chick.play();
+}
+
+var chicken = new Audio();
+chicken.src ="audio/chicken.mp3";
+function P7(){
+	chicken.play();
+}
+
+var cow = new Audio();
+cow.src ="audio/cow.mp3";
+function P8(){
+	cow.play();
+}
+
+var crane = new Audio();
+crane.src ="audio/crane.mp3";
+function P9(){
+	crane.play();
+}
+
+var cricket = new Audio();
+cricket.src ="audio/cricket.mp3";
+function P10(){
+	cricket.play();
+}
+
+var deer = new Audio();
+deer.src ="audio/deer.mp3";
+function P11(){
+	deer.play();
+}
+
+var dog = new Audio();
+dog.src ="audio/dog.mp3";
+function P12(){
+	dog.play();
+}
+
+var dolphin = new Audio();
+dolphin.src ="audio/dolphin.mp3";
+function P13(){
+	dolphin.play();
+}
+
+var donkey = new Audio();
+donkey.src ="audio/donkey.mp3";
+function P14(){
+	donkey.play();
+}
+
+var duck = new Audio();
+duck.src ="audio/duck.mp3";
+function P15(){
+	duck.play();
+}
+
+var eagle = new Audio();
+eagle.src ="audio/eagle.mp3";
+function P16(){
+	eagle.play();
+}
+
+var elephant = new Audio();
+elephant.src ="audio/elephant.mp3";
+function P17(){
+	elephant.play();
+}
+
+var fly = new Audio();
+fly.src ="audio/fly.mp3";
+function P18(){
+	fly.play();
+}
+
+var fox = new Audio();
+fox.src ="audio/fox.mp3";
+function P19(){
+	fox.play();
+}
+
+var frog = new Audio();
+frog.src ="audio/frog.mp3";
+function P20(){
+	frog.play();
+}
+
+var gecko = new Audio();
+gecko.src ="audio/gecko.mp3";
+function P21(){
+	gecko.play();
+}
+
+var giraffe = new Audio();
+giraffe.src ="audio/giraffe.mp3";
+function P22(){
+	giraffe.play();
+}
+
+var gorilla = new Audio();
+gorilla.src ="audio/gorilla.mp3";
+function P23(){
+	gorilla.play();
+}
+
+var hamster = new Audio();
+hamster.src ="audio/hamster.mp3";
+function P24(){
+	hamster.play();
+}
+
+var horse = new Audio();
+horse.src ="audio/horse.mp3";
+function P25(){
+	horse.play();
+}
+
+var humpback = new Audio();
+humpback.src ="audio/humpback.mp3";
+function P26(){
+	humpback.play();
+}
+
+var leopard = new Audio();
+leopard.src ="audio/leopard.mp3";
+function P27(){
+	leopard.play();
+}
+
+var limur = new Audio();
+limur.src ="audio/limur.mp3";
+function P28(){
+	limur.play();
+}
+
+var lion = new Audio();
+lion.src ="audio/lion.mp3";
+function P29(){
+	lion.play();
+}
+
+var llama = new Audio();
+llama.src ="audio/llama.mp3";
+function P30(){
+	llama.play();
+}
+
+var monkey = new Audio();
+monkey.src ="audio/monkey.mp3";
+function P31(){
+	monkey.play();
+}
+
+var mouse = new Audio();
+mouse.src ="audio/mouse.mp3";
+function P32(){
+	mouse.play();
+}
+
+var ostrich = new Audio();
+ostrich.src ="audio/ostrich.mp3";
+function P33(){
+	ostrich.play();
+}
+
+var owl = new Audio();
+owl.src ="audio/owl.mp3";
+function P34(){
+	owl.play();
+}
+
+var panda = new Audio();
+panda.src ="audio/panda.mp3";
+function P35(){
+	panda.play();
+}
+
+var parrot = new Audio();
+parrot.src ="audio/parrot.mp3";
+function P36(){
+	parrot.play();
+}
+
+var penguin = new Audio();
+penguin.src ="audio/penguin.mp3";
+function P37(){
+	penguin.play();
+}
+
+var pig = new Audio();
+pig.src ="audio/pig.mp3";
+function P38(){
+	pig.play();
+}
+
+var pigeon = new Audio();
+pigeon.src ="audio/pigeon.mp3";
+function P39(){
+	pigeon.play();
+}
+
+var polarbear = new Audio();
+polarbear.src ="audio/polarbear.mp3";
+function P40(){
+	polarbear.play();
+}
+
+var hamster = new Audio();
+hamster.src ="audio/hamster.mp3";
+function P41(){
+	hamster.play();
+}
+
+var racoon = new Audio();
+racoon.src ="audio/racoon.mp3";
+function P42(){
+	racoon.play();
+}
+
+var rhino = new Audio();
+rhino.src ="audio/rhino.mp3";
+function P43(){
+	rhino.play();
+}
+
+var rooster = new Audio();
+rooster.src ="audio/rooster.mp3";
+function P44(){
+	rooster.play();
+}
+
+var seal = new Audio();
+seal.src ="audio/seal.mp3";
+function P45(){
+	seal.play();
+}
+
+var sheep = new Audio();
+sheep.src ="audio/sheep.mp3";
+function P46(){
+	sheep.play();
+}
+
+var snake = new Audio();
+snake.src ="audio/snake.mp3";
+function P47(){
+	snake.play();
+}
+
+var squirrel = new Audio();
+squirrel.src ="audio/squirrel.mp3";
+function P48(){
+	squirrel.play();
+}
+
+var tiger = new Audio();
+tiger.src ="audio/tiger.mp3";
+function P49(){
+	tiger.play();
+}
+
+var zebra = new Audio();
+zebra.src ="audio/zebra.mp3";
+function P50(){
+	zebra.play();
+}
